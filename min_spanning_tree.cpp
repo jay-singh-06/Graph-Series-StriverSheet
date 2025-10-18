dde7b2f958f9b49d782b04b83f0da3929741f260
jay-singh-06 <js6652805@gmail.com>
2025-10-18 15:27:48 +0530

    day 31 of graph series

---
+++ min_spanning_tree.cpp
@@ -0,0 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//spanning tree=A tree in which we have n node and n-1 edges and all node are reachable from each other
+//minimum spanning tree menas any tree whose edge sum is lesser is known as mst
